e74d88420d4d409a66a29c1e20af7eb2390003bb
umang-jain <umang.usict.042164@ipu.ac.in>
2019-03-17 00:35:44 +0530

    video add

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar'
+import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
 
-  state={videos:[]}
+  state={videos:[], selectedVideo: null}
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo:video})
+  }
 
   onSearchSubmit = async (term) => {
     const response = await youtube.get('/search',{
@@ -18,7 +24,17 @@ class App extends React.Component {
   render(){
     return (
       <div className='ui container'>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <div className='ui grid'>
+          <div className='ui row'>
+            <div className='eleven wide column'>
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className='five wide column'>
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   };
